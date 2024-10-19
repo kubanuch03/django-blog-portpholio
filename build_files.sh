612d59f772a4d0ff231e754795acf2cd14ec4ef0
kubanuch03 <zazaka71@gmail.com>
2024-10-19 18:15:47 +0600

    Update vercel configure

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
-sudo apt-get install -y libsqlite3-dev
+apt-get update
+apt-get install -y libsqlite3-dev || true 
 
 # Проверка и установка pip, если он отсутствует
 if ! command -v pip &> /dev/null
