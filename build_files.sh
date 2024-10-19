c970e4b9635056c8b3ea76407725c0a62b9c9149
kubanuch03 <zazaka71@gmail.com>
2024-10-19 18:17:26 +0600

    Update vercel configure

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 set -e
 
-apt-get update
-apt-get install -y libsqlite3-dev || true 
-
-# Проверка и установка pip, если он отсутствует
-if ! command -v pip &> /dev/null
-then
-    echo "pip не найден. Устанавливаю pip..."
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py
-fi
-
 python3 -m pip install -r requirements.txt
 
 python3 manage.py collectstatic --noinput
