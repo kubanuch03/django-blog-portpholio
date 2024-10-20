c6cc16100ce4b914a1f43dd9a83268ab4d89a4af
kubanuch03 <zazaka71@gmail.com>
2024-10-20 23:20:55 +0600

    Add: docker and optimaze code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate 
 python manage.py collectstatic --noinput
-gunicorn blogApp.wsgi:application --bind 0.0.0.0:8001 --workers 4
+gunicorn blogApp.wsgi:application --bind 0.0.0.0:8002 --workers 4
